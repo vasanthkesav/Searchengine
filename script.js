56f9bac41b3e2f3ec945d4f395bd35ce4c650bf6
vasanthkesav <86461056+vasanthkesav@users.noreply.github.com>
2024-10-13 16:33:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ ZOHO.CREATOR.init()
       const list = document.querySelector(".list");
       let tag = "";
       for (let i = 0; i < all_items.length; i++) {
-        tag += `<a class="item text-dark" target='_blank'>${all_items[i].Name}</a>`;
+        tag += `<a href="www.google.com" class="item text-dark" target='_blank'>${all_items[i].Name}</a>`;
       }
       list.innerHTML = tag;
     }
